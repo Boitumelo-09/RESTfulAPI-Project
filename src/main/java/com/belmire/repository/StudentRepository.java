0292c6af3a242fbbc7d3cc2440bb319bae1fbd77
boitumelo-09 <belmiredub4@gmail.com>
2026-04-22 07:15:35 +0200

    Add StudentRepository interface and JPA dependency for database integration

---
+++ src/main/java/com/belmire/repository/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.belmire.repository;
+
+import com.belmire.models.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Integer> {
+
+
+}
